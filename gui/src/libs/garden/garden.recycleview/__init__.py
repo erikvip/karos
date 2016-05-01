12b362e563fbeddc95c446627a25f92eb50a2998
Erik Phillips <erikvip@gmail.com>
2016-05-01 03:44:51 -0600

    Moved recycleview to local user kivy garden folder

--- gui/src/libs/garden/garden.recycleview/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.1"
-from .recycleview import RecycleView, RecycleLayoutManager, \
-    LinearRecycleLayoutManager, RecycleAdapter, RecycleViewMixin, \
-    LayoutChangeException, LayoutSelectionMixIn, RecycleViewLayout
